c7506ba1c4c8ee9595176516950bcc1c3c5fd90e
u45u123 <u45u123@naver.com>
2025-11-15 19:48:34 +0900

    ass 4-2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Hello/>
+
+        <h1 style={{color: 'yellow', backgroundColor: 'purple'}}>제목</h1>
+
+        <ul style={{textAlign: 'left', color: 'pink'}}>
+          <li>사과</li>
+          <li>바나나</li>
+          <li>딸기</li>
+        </ul>
+
+        <button style={{fontSize: '20px', padding: '10px', marginTop: '10px'}}>눌러보세요</button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import './Hello.css';
+
+function Hello() {
+  return (
+    <div className="hello">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default Hello;
